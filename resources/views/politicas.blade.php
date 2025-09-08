e83b9361484548932298456df8036fe2d617217e
jhosyadriana11 <jhosyrodriguez99@gmail.com>
2025-09-07 22:39:23 -0600

    cambios 9

--- resources/views/politicas.blade.php
+++ resources/views/politicas.blade.php
@@ -22,41 +22,46 @@
 				a la recopilación, uso y divulgación de datos personales cuando utiliza nuestro servicio y las opciones 
 				que tiene asociadas con esos datos.</p>
 			<br />
-			<p>Recopilación y uso de información</p>
+			<h5>Recopilación y uso de información</h5>
 			<p class="lead linea-izq">Recopilamos varios tipos de información con diferentes propósitos para proporcionar y mejorar nuestro servicio.</p>
 			<br />
-			<p>Tipos de datos recopilados</p>
-			<p>Datos personales</p>
-			<p class="lead linea-izq">Al usar nuestro servicio, podemos solicitarle que nos proporcione cierta información de identificación personal 
-				que puede incluir, entre otros, su nombre, dirección de correo electrónico, número de teléfono y otra información 
-				que pueda ser necesaria para proporcionarle los servicios solicitados o mejorar su experiencia con nosotros.</p>
+			<h5>Tipos de datos recopilados:</h5>
+			<ul>
+				<li>
+					<h6>Datos personales</h6>
+					<p class="lead linea-izq">Al usar nuestro servicio, podemos solicitarle que nos proporcione cierta información de identificación personal 
+						que puede incluir, entre otros, su nombre, dirección de correo electrónico, número de teléfono y otra información 
+						que pueda ser necesaria para proporcionarle los servicios solicitados o mejorar su experiencia con nosotros.</p>
+				</li>
+				<li>
+					<h6>Datos de uso</h6>
+					<p class="lead linea-izq">También podemos recopilar información sobre cómo accede y utiliza nuestro sitio ("Datos de uso"). Estos datos 
+						de uso pueden incluir información como la dirección del protocolo de internet de su computadora 
+						(por ejemplo, dirección IP), tipo de navegador, versión del navegador, las páginas de nuestro servicio 
+						que visita, la hora y la fecha de su visita, el tiempo que pasa en esas páginas, identificadores únicos 
+						de dispositivo y otros datos de diagnóstico.</p>
+						</li>
+			</ul>
 			<br />
-			<p>Datos de uso</p>
-			<p class="lead linea-izq">También podemos recopilar información sobre cómo accede y utiliza nuestro sitio ("Datos de uso"). Estos datos 
-				de uso pueden incluir información como la dirección del protocolo de internet de su computadora 
-				(por ejemplo, dirección IP), tipo de navegador, versión del navegador, las páginas de nuestro servicio 
-				que visita, la hora y la fecha de su visita, el tiempo que pasa en esas páginas, identificadores únicos 
-				de dispositivo y otros datos de diagnóstico.</p>
-			<br />
-			<p>Seguimiento y cookies</p>
+			<h5>Seguimiento y cookies</h5>
 			<p class="lead linea-izq">Utilizamos cookies y tecnologías de seguimiento similares para rastrear la actividad en nuestro servicio 
-				y mantener cierta información</p>
+				y mantener cierta información.</p>
 			<br />
-			<p>Cookies</p>
+			<h5>Cookies</h5>
 			<p class="lead linea-izq">Las cookies son archivos con una pequeña cantidad de datos que pueden incluir un identificador único anónimo. 
 				Las cookies se envían a su navegador desde un sitio web y se almacenan en su dispositivo. 
 				También utilizamos otras tecnologías de seguimiento, como balizas, etiquetas y scripts, para recopilar 
 				y rastrear información y para mejorar y analizar nuestro servicio.</p>
 			<br />
-			<p>Ejemplos de cookies que utilizamos</p>
+			<h5>Ejemplos de cookies que utilizamos:</h5>
 			<p>
 				<ul>
 					<li>
-						<p>Cookies de preferencias</p>
+						<h6>Cookies de preferencias</h6>
 						<p class="lead linea-izq">Utilizamos cookies de preferencias para recordar sus preferencias y diversas configuraciones.</p>
 					</li>
 					<li>
-						<p>Cookies de seguridad</p>
+						<h6>Cookies de seguridad</h6>
 						<p class="lead linea-izq">Utilizamos cookies de seguridad para fines de seguridad. Puede instruir a su navegador para 
 							que rechace todas las cookies o para que le indique cuándo se está enviando una cookie. 
 							Sin embargo, si no acepta cookies, es posible que no pueda utilizar algunas partes de nuestro servicio.</p>
@@ -64,35 +69,35 @@
 				</ul>
 			</p>
 			<br />
-			<p>Proveedores de servicios</p>
+			<h5>Proveedores de servicios</h5>
 			<p class="lead linea-izq">Podemos emplear empresas e individuos de terceros para facilitar nuestro servicio ("Proveedores de servicios"), 
 				para proporcionar el servicio en nuestro nombre, para realizar servicios relacionados con el servicio o para 
 				ayudarnos a analizar cómo se utiliza nuestro servicio. Estos terceros tienen acceso a sus datos personales 
 				solo para realizar estas tareas en nuestro nombre y están obligados a no divulgarlos ni utilizarlos para 
 				ningún otro propósito.</p>
 			<br />
-			<p>Seguridad</p>
+			<h5>Seguridad</h5>
 			<p class="lead linea-izq">La seguridad de sus datos personales es importante para nosotros, pero recuerde que ningún método de 
 				transmisión por Internet o método de almacenamiento electrónico es 100% seguro. Aunque nos esforzamos 
 				por utilizar medios comercialmente aceptables para proteger sus datos personales, no podemos garantizar 
 				su seguridad absoluta.</p>
 			<br />
-			<p>Enlaces a otros sitios</p>
+			<h5>Enlaces a otros sitios</h5>
 			<p class="lead linea-izq">Nuestro servicio puede contener enlaces a otros sitios que no son operados por nosotros. 
 				Si hace clic en un enlace de un tercero, será dirigido al sitio de ese tercero. 
 				Le recomendamos encarecidamente que revise la Política de Privacidad de cada sitio que visite.</p>
 			<br />
-			<p>Cambios a esta política de privacidad</p>
+			<h5>Cambios a esta política de privacidad</h5>
 			<p class="lead linea-izq">Podemos actualizar nuestra Política de privacidad de vez en cuando. Le notificaremos cualquier cambio 
 				publicando la nueva Política de Privacidad en esta página. Se le recomienda revisar esta Política de 
 				Privacidad periódicamente para cualquier cambio. Los cambios a esta Política de privacidad entran 
 				en vigencia cuando se publican en esta página.</p>
 			<br />
-			<p>Preguntas</p>
+			<h5>Preguntas</h5>
 			<p class="lead linea-izq">Si tiene alguna pregunta sobre esta Política de Privacidad, puede ponerse en contacto con nosotros:</p>
 			<p>
 				<ul>
-					<li>Mediante el formulario de contacto disponible en nuestro sitio</li>
+					<li class="lead">Mediante el formulario de contacto disponible en nuestro sitio</li>
 				</ul>
 			</p>
 			</div>
